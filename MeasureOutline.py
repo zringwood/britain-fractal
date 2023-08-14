7d6de41a6ef4e5ebe548cffb1f8734df380156dc
Zachary Ringwood <zringwood@gmail.com>
2023-08-14 17:27:50 -0400

    Working on sorting the array

--- MeasureOutline.py
+++ MeasureOutline.py
@@ -1,8 +1,20 @@
-import pickle, sys
-from PIL import Image
+import pickle, sys, math
+from PIL import Image, ImageDraw
 
 outline = pickle.load(open('output.pkl', 'rb'))
-mask = Image.open("mask.png")
-radius = sys.argv[1]
+outline.pop(0)
+outline.pop(0)
+outline.pop(0)
+radius = (int)(sys.argv[1])
+points = [outline[0]]
+for i in range(len(outline)) : 
+    for point in outline: 
+        if math.dist(point, points[0]) == 1 and points.count(point) == 0 :
+            points.insert(0, point)
 
-
+mask = Image.open("mask-small.png")
+draw = ImageDraw.Draw(mask)
+# draw.polygon(points, None, (255,0,0), 10)
+for point in points:
+    mask.paste((0, 0, 255, 255), (point[0], point[1], point[0] + 5, point[1] + 5))
+mask.show()
--- masking/GetOutline.py
+++ masking/GetOutline.py
@@ -33,13 +33,12 @@ def removeBlackPixels(path) :
     for pixel in path :
         point = (pixel[0], pixel[1])
         if not isOutOfBounds(point) and outline.getpixel(point) == WHITE :
-
             retpath.append(pixel)
     return retpath
 def removeDuplicates(path) :
    for element in path :
        if path.count(element) > 1 :
-           path.remove
+           path.remove(element)
 endpoint = (384, 234)
 startpoint = (384, 236)
 path = [startpoint]
@@ -53,7 +52,9 @@ while path[len(path)-1] != endpoint:
         if path.count(neighbour) == 0 and outline.getpixel(neighbour) == WHITE and countBlackNeighbours(neighbour[0], neighbour[1]) > 0 :
             path.append(neighbour)
             break
-
+    index += 1
+    if index > 10000 : 
+        break
 for pixel in path :
     outline.putpixel((pixel[0], pixel[1]), (255,0,0))
 outline.show()
